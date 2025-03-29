ed73d7ac5379f59b426ab7813b010b041b33de48
chandubhati1997 <chandreveersingh@gmail.com>
2025-03-30 02:17:45 +0530

    portfolio is readdy

--- utils/lazyloading.js
+++ utils/lazyloading.js
@@ -1,21 +1,21 @@
-// const lazyLoading = () => {
-//     const lazyImgs = document.querySelectorAll('.lazy');
+const lazyLoading = () => {
+    const lazyImgs = document.querySelectorAll('.lazy');
   
-//     const observer = new IntersectionObserver((entries, observer) => {
-//       entries.forEach(entry => {
-//         if (entry.isIntersecting) {
-//           let img = entry.target;
-//           img.src = img.dataset.src;
-//           img.classList.remove('loading');
-//           img.classList.add('loaded');
-//           observer.unobserve(img);
-//         }
-//       });
-//     });
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          let img = entry.target;
+          img.src = img.dataset.src;
+          img.classList.remove('loading');
+          img.classList.add('loaded');
+          observer.unobserve(img);
+        }
+      });
+    });
   
-//     lazyImgs.forEach(img => {
-//       observer.observe(img);
-//     });
-//   };
+    lazyImgs.forEach(img => {
+      observer.observe(img);
+    });
+  };
   
-//   export default lazyLoading;
+  export default lazyLoading;
